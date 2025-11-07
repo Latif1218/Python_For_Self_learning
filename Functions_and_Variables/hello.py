779a38c5e8eef0cc17e105e4b9a819070482dd59
Latif <mdsabdullotif@gmail.com>
2025-11-07 17:04:58 +0600

    add this

---
+++ Functions_and_Variables/hello.py
@@ -0,0 +1,21 @@
+# input("what's your name? ")
+
+# print("hello, latif")
+
+## that is only prinf my name. not others 
+## so what should i do to print any persons name?
+
+
+#___________________________________________________________________________
+
+# the solution is let's define variable as the name of (name)
+
+name = input("what's your name? ")
+
+print("hello, "+ name)
+
+## naw it's print any name
+
+name = input("what's your name? ")
+
+print("hello,",name)
