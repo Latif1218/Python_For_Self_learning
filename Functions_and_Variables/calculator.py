dd1a7a2b843f86f9d754987c764deda37733773d
Latif <mdsabdullotif@gmail.com>
2025-11-08 15:32:25 +0600

    add

---
+++ Functions_and_Variables/calculator.py
@@ -0,0 +1,49 @@
+## type1
+
+# x = 2
+# y = 3
+# z = x + y
+
+# print(z)
+
+## type2
+
+# x = int(input("what's x? "))
+# y = int(input("What's y? "))
+
+# print(x + y)
+
+
+
+## type3
+
+# print(int(input("what's x? ")) + int(input("What's y? ")))
+
+
+# print(float(input("what's x? ")) + float(input("What's y? ")))
+
+
+
+# x = float(input("what's x? "))
+# y = float(input("What's y? "))
+
+# z = round(x+y)
+
+# print(z)
+
+
+# x = float(input("what's x? "))
+# y = float(input("What's y? "))
+
+# z = round(x+y)
+
+# print(f"{z:,}")
+
+
+
+x = float(input("what's x? "))
+y = float(input("What's y? "))
+
+z = round(x/y)
+
+print(f"{z:.2f}")
--- Functions_and_Variables/hello.py
+++ Functions_and_Variables/hello.py
@@ -6,20 +6,76 @@
 ## so what should i do to print any persons name?
 
 
-#___________________________________________________________________________
+# #___________________________________________________________________________
 
-# the solution is let's define variable as the name of (name)
+# # the solution is let's define variable as the name of (name)
 
-name = input("what's your name? ")
+# # ask user for their name
 
-print("hello, "+ name)
+# name = input("what's your name? ")
 
-print("hello,",name)
+# # say hello to user
+
+# print("hello, "+ name)
+# print("hello,",name)
+
+# print("hello, ",end='')
+# print(name)
 
 ## naw it's print any name
 
 
 
 
+# name = input("what's your name? ")
+# print(f"hello, {name}")
 
 
+
+
+
+
+# name = input("what's your name? ")
+
+
+# # Remove writespace from str
+# name = name.strip()
+
+
+# #Capitalize user's name 
+# name = name.title()
+
+
+# print(f"hello, {name}")
+
+
+
+
+
+# name = input("what's your name? ").strip().title()
+# print(f"hello, {name}")
+
+
+
+
+# name = input("what's your name? ").strip().title()
+
+# # split user's name into first name and last name
+# first, last = name.split(" ")
+
+# print(f"hello, {first}")
+
+
+
+
+
+
+# function 
+def hello(to):
+    print("hello,", to)
+    
+
+name = input("What's your name? ")
+hello(name) 
+
+  
